b503a52b50006a759c13688ba7e504e11d6d744a
LE DINH Alban <79143540+LEDINHAlban@users.noreply.github.com>
2025-01-29 16:17:56 +0100

    feat: add real data work experience

--- components/CardExperience.tsx
+++ components/CardExperience.tsx
@@ -1,40 +1,41 @@
 import Image from "next/image";
 import React from "react";
 
-function CardExperience() {
+type Experience = {
+  company: string;
+  role: string;
+  date: string;
+  description: string[];
+  logo: { path: string; width: number; height: number };
+  stacks: string[];
+};
+
+function CardExperience({ data }: { data: Experience }) {
   return (
-    <article className="bg-[#282828]">
-      <div className="flex justify-center items-center mt-5">
+    <article className="bg-[#282828] rounded-lg shadow-lg p-5">
+      <div className="flex justify-center items-center mt-5 ">
         <Image
-          src="/acheterlouer_logo.png"
-          height={100}
-          width={100}
+          src={data.logo.path}
+          height={data.logo.height}
+          width={data.logo.width}
           alt={"compagny logo"}
         />
       </div>
       <div className="flex flex-col space-y-1 mt-5">
-        <h3 className="text-4xl font-light">Company</h3>
-        <p className="font-bold text-2xl">Role</p>
+        <h3 className="text-4xl font-light">{data.company}</h3>
+        <p className="font-bold text-2xl">{data.role}</p>
         <div className="flex space-x-2">
-          <Image
-            src="/stack_logo/react.svg"
-            height={40}
-            width={40}
-            alt={"react"}
-          />
-          <Image
-            src="/stack_logo/vuejs.svg"
-            height={40}
-            width={40}
-            alt={"vuejs"}
-          />
+          {data.stacks.map((stack) => (
+            <Image key={stack} src={stack} height={40} width={40} alt={stack} />
+          ))}
         </div>
       </div>
 
-      <p className="mt-7 uppercase text-sm text-gray-300">Duration</p>
+      <p className="mt-7 uppercase text-sm text-gray-300">{data.date}</p>
       <ul className="pl-4 space-y-4 text-sm mt-3 list-disc">
-        <li>first comment fdsqf dsqfqsf sdf fdsqfqs dfqsd fqsfd</li>
-        <li>second comment</li>
+        {data.description.map((desc) => (
+          <li key={desc}>{desc}</li>
+        ))}
       </ul>
     </article>
   );
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -5,6 +5,52 @@ import React from "react";
 import { motion } from "motion/react";
 
 function WorkExperience() {
+  const experience = [
+    {
+      company: "Capgemini Engineering",
+      role: "Software Engineer",
+      date: "2022 - 2023",
+      description: [
+        "Participation in SCRUM projects, managing tasks through JIRA for sprint tracking and deliverables.",
+        "Ensuring adherence to UI/UX mockups to maintain consistency.",
+        "Proposing patterns for data management and component hydration.",
+        "Creating reusable components for multiple projects.",
+        "Writing unit tests and integration tests using React Testing Library, as well as snapshots.",
+        "Developing a two-way matrix with customizable cells to display participant privileges.",
+        "Designing and integrating forms using React Hook Form.",
+        "Creating charts with ChartJS for event data visualization.",
+        "Conducting code reviews to ensure quality and adherence to best practices.",
+      ],
+      logo: {
+        path: "/capgemini_Engineering_logo.webp",
+        height: 400,
+        width: 400,
+      },
+      stacks: [
+        "/stack_logo/react.svg",
+        "/stack_logo/jira.svg",
+        "/stack_logo/vsc.svg",
+      ],
+    },
+    {
+      company: "Acheter Louer",
+      role: "Web developper",
+      date: "2021 - 2022",
+      description: [
+        "Website update for acheter-louer.fr",
+        "VueJS interface: End-to-end development with a NestJS and TypeORM backend, data management, and automation of press release publication. Implemented JWT authentication to secure access.",
+        "ReactJS interface: Created a CMS for article writing and user management, with a FeathersJS backend. Designed interfaces using Figma, from mockup to implementation.",
+      ],
+      logo: { path: "/acheterlouer_logo.png", height: 150, width: 150 },
+      stacks: [
+        "/stack_logo/react.svg",
+        "/stack_logo/vuejs.svg",
+        "/stack_logo/nestjs.svg",
+        "/stack_logo/figma.svg",
+        "/stack_logo/vsc.svg",
+      ],
+    },
+  ];
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -17,8 +63,9 @@ function WorkExperience() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 gap-y-8 mt-8 max-w-7xl mx-auto p-8">
-        <CardExperience />
-        <CardExperience />
+        {experience.map((experience, index) => (
+          <CardExperience key={index} data={experience} />
+        ))}
       </div>
     </motion.div>
   );
