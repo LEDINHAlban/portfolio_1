8fd63e70d1d63d7cabe5b82b631efb8d53de47d0
LE DINH Alban <79143540+LEDINHAlban@users.noreply.github.com>
2025-01-29 17:18:43 +0100

    feat: add more skills

--- README.md
+++ README.md
@@ -38,3 +38,4 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/bui
 ### Use of
 
 - [React icons](https://react-icons.github.io/react-icons/)
+- [Tech logos](https://techicons.dev/)
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -14,13 +14,13 @@ function Skills() {
       title: "Figma",
       name: "figma.svg",
       direction: Direction.Left,
-      mastering: 40,
+      mastering: 50,
     },
     {
       title: "Jira",
       name: "jira.svg",
       direction: Direction.Left,
-      mastering: 40,
+      mastering: 70,
     },
     { title: "JS", name: "js.svg", direction: Direction.Left, mastering: 80 },
     {
@@ -32,32 +32,92 @@ function Skills() {
     {
       title: "React",
       name: "react.svg",
-      direction: Direction.Right,
+      direction: Direction.Left,
       mastering: 80,
     },
     {
       title: "VSC",
       name: "vsc.svg",
-      direction: Direction.Right,
+      direction: Direction.Left,
       mastering: 80,
     },
     {
       title: "Vuejs",
       name: "vuejs.svg",
-      direction: Direction.Right,
-      mastering: 20,
+      direction: Direction.Left,
+      mastering: 40,
     },
     {
       title: "Nodejs",
       name: "nodejs.svg",
+      direction: Direction.Left,
+      mastering: 40,
+    },
+    {
+      title: "CSS3",
+      name: "css3.svg",
+      direction: Direction.Right,
+      mastering: 80,
+    },
+    {
+      title: "Docker",
+      name: "docker.svg",
       direction: Direction.Right,
       mastering: 40,
+    },
+    {
+      title: "FeathersJS",
+      name: "feathers.svg",
+      direction: Direction.Right,
+      mastering: 40,
+    },
+    {
+      title: "GraphQL",
+      name: "graphql.svg",
+      direction: Direction.Right,
+      mastering: 40,
+    },
+    {
+      title: "MongoDB",
+      name: "mongodb.svg",
+      direction: Direction.Right,
+      mastering: 40,
+    },
+    {
+      title: "MySQL",
+      name: "mysql.svg",
+      direction: Direction.Right,
+      mastering: 40,
+    },
+    {
+      title: "Nextjs",
+      name: "nextjs.svg",
+      direction: Direction.Right,
+      mastering: 80,
+    },
+    {
+      title: "Postgres",
+      name: "postgressql.svg",
+      direction: Direction.Right,
+      mastering: 50,
+    },
+    {
+      title: "Tailwind",
+      name: "tailwind.svg",
+      direction: Direction.Right,
+      mastering: 70,
+    },
+    {
+      title: "TypeScript",
+      name: "typescript.svg",
+      direction: Direction.Right,
+      mastering: 80,
     },
   ];
   return (
     <div className="h-screen grayBackground flex flex-col items-center">
       <h2 className="pageTitle mt-12">Skills</h2>
-      <div className="grid grid-cols-4 gap-4 mt-20 p-[50px]">
+      <div className="grid grid-cols-4 gap-4 mt-20 md:mt-0 p-[50px]">
         {skills.map((skill) => {
           return (
             <motion.div
@@ -87,9 +147,12 @@ function Skills() {
                 width={80}
                 className="rounded-full border-solid border-2"
               />
-              <div className="absolute inset-0 bg-white opacity-0 group-hover:opacity-80 flex items-center justify-center rounded-full transition-opacity duration-300">
-                <span className="text-black text-xl font-bold">
-                  {skill.mastering}%
+              <div className="absolute inset-0 bg-white opacity-0 group-hover:opacity-80 flex flex-col items-center justify-center rounded-full transition-opacity duration-300">
+                <span className="text-black text-sm font-bold text-center">
+                  {`${skill.title}`}
+                </span>
+                <span className="text-black text-xl font-bold text-center">
+                  {`${skill.mastering}%`}
                 </span>
               </div>
             </motion.div>
