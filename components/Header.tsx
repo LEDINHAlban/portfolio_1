1f9a3d5c2af106d87e9388fcf7fa11a1738d5eca
LE DINH Alban <79143540+LEDINHAlban@users.noreply.github.com>
2025-01-30 11:46:03 +0100

    fix: header css

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,8 +9,8 @@ import { motion } from "motion/react";
 
 function Header() {
   return (
-    <header className="sticky top-0 border-[#333333] z-50 max-w-7xl mx-auto p-4">
-      <div className="flex justify-between">
+    <header className="sticky top-0 bg-transparent z-50 p-4">
+      <div className="flex justify-between items-center max-w-7xl mx-auto">
         <motion.div
           initial={{ x: -70, opacity: 0, scale: 0 }}
           animate={{ x: 0, opacity: 1, scale: 1 }}
@@ -18,10 +18,10 @@ function Header() {
           className="flex space-x-4"
         >
           <Link href="https://github.com/LEDINHAlban">
-            <FaGithub color="grey" size="1.2em" />
+            <FaGithub color="grey" size="1.2em" className="icons" />
           </Link>
           <Link href="https://www.linkedin.com/in/a-ledinh/">
-            <FaLinkedin color="grey" size="1.2em" />
+            <FaLinkedin color="grey" size="1.2em" className="icons" />
           </Link>
         </motion.div>
         <motion.div
@@ -33,7 +33,7 @@ function Header() {
             href={"#contact"}
             className="heroButton flex space-x-3 items-center"
           >
-            <TbMailFilled color="grey" size="1.2em" />
+            <TbMailFilled color="grey" size="1.2em" className="icons" />
             <p className="hidden md:block">Get in touch</p>
           </Link>
         </motion.div>
