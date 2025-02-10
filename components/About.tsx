4d48c5862d3bb96f338cfdad609e1ec568670c7a
LE DINH Alban <79143540+LEDINHAlban@users.noreply.github.com>
2025-02-10 20:15:39 +0100

    feat: add animation on about page

--- components/About.tsx
+++ components/About.tsx
@@ -1,39 +1,84 @@
 "use client";
 
+import AboutContent from "./AboutContent";
 import { motion } from "motion/react";
 
+const draw = {
+  hidden: { pathLength: 0, opacity: 0 },
+  visible: (i: number) => {
+    const delay = i * 0.1;
+    return {
+      pathLength: 1,
+      opacity: 1,
+      transition: {
+        pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+        opacity: { delay, duration: 0.01 },
+      },
+    };
+  },
+};
+
 const About = () => {
+  const image: React.CSSProperties = {
+    maxWidth: "90vw",
+  };
+
+  const shape: React.CSSProperties = {
+    strokeWidth: 10,
+    strokeLinecap: "round",
+    fill: "transparent",
+  };
+
   return (
-    <div className="min-h-screen grayBackground flex flex-col items-center">
-      <h2 className="pageTitle mt-12 mb-12">About</h2>
-      <div className="h-full w-full flex flex-col items-center justify-center p-5 sm:mt-24">
-        <div className="max-w-6xl flex flex-col md:flex-row items-center text-center md:text-left">
-          <motion.div
-            initial={{ x: -200, opacity: 0 }}
-            whileInView={{ x: 0, opacity: 1 }}
-            transition={{ duration: 1, ease: "easeOut" }}
-            viewport={{ amount: 0.3 }}
-            className="md:ml-8 mt-6 md:mt-0 text-center md:text-left px-6"
+    <div className="h-screen grayBackground flex flex-col items-center justify-center sm:pt-20">
+      <h2 className="pageTitle sm:mt-0 mb-12">About</h2>
+      <div className="min-h-0 w-full flex flex-col items-center justify-center p-5">
+        <div className="max-w-6xl flex flex-col items-center text-center">
+          <motion.svg
+            width="900"
+            height="900"
+            viewBox="0 0 900 900"
+            initial="hidden"
+            whileInView="visible"
+            style={image}
+            viewport={{ once: false, amount: 0.3 }}
+            className="hidden sm:block" // Hide animation on mobile
           >
-            <h3 className="text-xl font-bold">Here is a little background</h3>
-            <p className="mt-4 text-gray-300 text-sm md:text-base leading-relaxed">
-              I&apos;m Alban. I&apos;ve been coding for over 2 years now. As a
-              Full Stack developer, I&apos;ve worked with a real-estate and
-              event management companies.
-              <br />
-              <br />
-              I&apos;m a passionate developer specializing in{" "}
-              <strong>ReactJS</strong> and
-              <strong> TypeScript</strong>, I am just beginning to explore the
-              world of <strong> Next.js</strong> with great enthusiasm. I have
-              completed several projects involving data management operations.
-              <br />
-              <br />I am sensitive to the impact of technology on the
-              <strong> environment</strong>. Although I am still expanding my
-              knowledge in this field, I am determined to contribute to more
-              sustainable technological solutions.
-            </p>
-          </motion.div>
+            {/* Big screen */}
+            <motion.rect
+              width="700"
+              height="350"
+              x="100"
+              y="100"
+              rx="20"
+              stroke="#808080"
+              variants={draw}
+              custom={5}
+              style={shape}
+            />
+            {/* Keyboard */}
+            <motion.rect
+              width="700"
+              height="80"
+              x="100"
+              y="470"
+              rx="20"
+              stroke="#808080"
+              variants={draw}
+              custom={6}
+              style={shape}
+            />
+            <foreignObject x="110" y="110" width="680" height="400">
+              <div className="text-gray-300 text-lg md:text-xl leading-relaxed p-4 overflow-y-auto">
+                <AboutContent />
+              </div>
+            </foreignObject>
+          </motion.svg>
+
+          {/* On mobile, only display text without the animation */}
+          <div className="sm:hidden text-gray-300 text-lg md:text-xl leading-relaxed p-4">
+            <AboutContent />
+          </div>
         </div>
       </div>
     </div>
---
+++ components/AboutContent.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function AboutContent() {
+  return (
+    <>
+      <h3 className="text-xl font-bold text-center">
+        Here is a little background
+      </h3>
+      <p className="mt-4 text-gray-300 text-sm md:text-base leading-relaxed text-center">
+        I&apos;m Alban. I&apos;ve been coding for over 2 years now. As a Full
+        Stack developer, I&apos;ve worked with real-estate and event management
+        companies.
+        <br />
+        <br />
+        I&apos;m a passionate developer specializing in
+        <strong> ReactJS</strong> and <strong>TypeScript</strong>, I am just
+        beginning to explore the world of
+        <strong> Next.js</strong> with great enthusiasm. I have completed
+        several projects involving data management operations.
+        <br />
+        <br />I am sensitive to the impact of technology on the
+        <strong> environment</strong>. Although I am still expanding my
+        knowledge in this field, I am determined to contribute to more
+        sustainable technological solutions.
+      </p>
+    </>
+  );
+}
+
+export default AboutContent;
